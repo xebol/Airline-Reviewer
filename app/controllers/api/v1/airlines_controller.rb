f1c0e811d3a8d103c2c2021972cffd886a591a2a
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-07-10 14:20:29 -0600

    add options method

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -4,35 +4,32 @@ module Api
       def index
         airlines = Airline.all
 
-        render json: AirlineSerializer.new(airlines).serialized_json
+        render json: AirlineSerializer.new(airlines, options).serialized_json
       end
-
 
       def show
         airline = Airline.find_by(slug: params[:slug])
 
-        render json: AirlineSerializer.new(airline).serialized_json
+        render json: AirlineSerializer.new(airline, options).serialized_json
       end
-
 
       def create
         airline = Airline.new(airline_params)
 
         if airline.save
           render json: AirlineSerializer.new(airline).serialized_json
-        else 
-          render json: {error: airline.errors.messages}, status: 422
+        else
+          render json: { error: airline.errors.messages }, status: 422
         end
       end
-
 
       def update
         airline = Airline.find_by(airline_params[:slug])
 
         if airline.update(airline_params)
-          render json: AirlineSerializer.new(airline).serialized_json
-        else 
-          render json: {error: airline.errors.messages}, status: 422
+          render json: AirlineSerializer.new(airline, options).serialized_json
+        else
+          render json: { error: airline.errors.messages }, status: 422
         end
       end
 
@@ -41,8 +38,8 @@ module Api
 
         if airline.destroy
           head :no_content
-        else 
-          render json: {error: airline.errors.messages}, status: 422
+        else
+          render json: { error: airline.errors.messages }, status: 422
         end
       end
 
@@ -50,6 +47,10 @@ module Api
 
       def airline_params
         params.require(:airline).permit(:name, :image_url)
+      end
+
+      def options
+        @options ||= {include: %i[reviews]}
       end
     end
   end
