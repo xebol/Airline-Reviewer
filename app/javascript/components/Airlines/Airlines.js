731c43e6da17abc6e0f773d686102507cb5947a6
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-07-14 18:10:21 -0600

    API request using axios

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,10 +1,27 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 
 const Airlines = () => {
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    //get all of the airlines from the API
+    //update airlines in our state
+
+
+    axios.get('/api/v1/airlines.json')
+    .then((response) => {
+      console.log('Response', response)
+    })
+    .catch((err) => {
+      console.log('Error', err)
+    })
+  }, [airlines.length])
+
   return (
     <div>This is the Airlines Component</div>
-  )
-}
+  );
+};
 
 
 export default Airlines;
