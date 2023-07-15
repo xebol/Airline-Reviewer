9622a4f7ec8b257b27996ca2330f93756dd25f53
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-07-15 14:17:18 -0600

    render all the data of each airlines

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Airline from "./Airline"
+import styled from "styled-components"
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Header = styled.div`
+  padding: 100px 100px 10px 100px
+  h1 {
+    font-size: 42px;
+  }
+`
+
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`
+
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
@@ -19,24 +46,26 @@ const Airlines = () => {
       });
   }, [airlines.length]);
 
-  const list = airlines.map((item) => {
+  const grid = airlines.map((item) => {
     return (
-      <li key={item.attributes.name}>
-        {item.attributes.name}
-      </li>
+      <Airline key={item.attributes.name}
+      attributes={item.attributes}
+      >
+       
+      </Airline>
     );
   });
 
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>Open Flights</h1>
-        <div className="subheader">Honest, unbiased airline reviews.</div>
-      </div>
-      <div className="grid">
-        <ul>{list}</ul>
-      </div>
-    </div>
+        <Subheader>Honest, unbiased airline reviews.</Subheader>
+      </Header>
+      <Grid>
+        <ul>{grid}</ul>
+      </Grid>
+    </Home>
   );
 };
 
