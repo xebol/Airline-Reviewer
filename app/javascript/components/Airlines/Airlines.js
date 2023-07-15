465a133406c68697124d7c9e5725edd116b8ea8b
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-07-14 18:20:34 -0600

    render the list of Airlines on the front end

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,25 +1,36 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Airlines = () => {
-  const [airlines, setAirlines] = useState([])
+  const [airlines, setAirlines] = useState([]);
 
   useEffect(() => {
     //get all of the airlines from the API
     //update airlines in our state
-
-
+    
     axios.get('/api/v1/airlines.json')
-    .then((response) => {
-      console.log('Response', response)
-    })
-    .catch((err) => {
-      console.log('Error', err)
-    })
-  }, [airlines.length])
+      .then((response) => {
+        console.log('Response', response.data.data);
+        setAirlines(response.data.data);
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
+  }, [airlines.length]);
+
+  const list = airlines.map((item) => {
+    return (
+      <li key={item.attributes.name}>
+        {item.attributes.name}
+      </li>
+    );
+  });
 
   return (
-    <div>This is the Airlines Component</div>
+    <>
+      <div>This is the Airlines Component</div>
+      <ul>{list}</ul>
+    </>
   );
 };
 
