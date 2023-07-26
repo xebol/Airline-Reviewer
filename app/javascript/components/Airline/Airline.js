bb003c85498adb911a023ac9b984ab264871dc21
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-07-26 16:45:08 -0600

    request data from the backend for each airline

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,8 +1,47 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Header from "./Header";
 
 const Airline = () => {
+  const [airline, setAirline] = useState({});
+  const [review, setReview] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+
+  const { slug } = useParams();
+
+  useEffect(() => {
+    const url = `/api/v1/airlines/${slug}`;
+
+    axios.get(url)
+      .then((response) => {
+        setAirline(response.data);
+        setLoaded(true);
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+
+  }, []);
+
   return (
-    <div>This is the Airline Component</div>
+    <div className="wrapper">
+
+      <div className="column-left">
+        {loaded &&
+          <Header
+            attributes={airline.data.attributes}
+            reviews={airline.included}
+          />
+        }
+        <div className="reviews"></div>
+      </div>
+      <div className="column-right">
+        <div className="review-form">Review Form Goes Here</div>
+      </div>
+
+    </div>
   );
 };
 
