4b5f0e0ed9e048de21a45f3d1a9594adcdc3a42f
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-07-26 16:45:38 -0600

    show airline details

---
+++ app/javascript/components/Airline/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = (props) => {
+  const { name, image_url, avg_score } = props.attributes;
+  const total = props.reviews.length;
+
+  return (
+    <div className="wrapper">
+      <h1><img src={image_url} alt={name} />{name}</h1>
+      <div>
+        <div className="total-reviews">{total} User Reviews</div>
+        <div className="star-rating"></div>
+        <div className="total-out-of">{avg_score} out of 5</div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Header;
