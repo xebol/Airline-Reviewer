c86879972e31f9301f911d8f7148de7216a40178
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-07-26 17:19:54 -0600

    add styling

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -2,6 +2,27 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "./Header";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+margin-left: auto;
+margin-right: auto;
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+`;
+const Column = styled.div`
+background: #fff;
+height: 100vh;
+overflow: scroll;
+
+
+&:last-child {
+  background: #000;
+}
+`;
+const Main = styled.div`
+padding-left: 50px;
+`;
 
 const Airline = () => {
   const [airline, setAirline] = useState({});
@@ -26,22 +47,24 @@ const Airline = () => {
   }, []);
 
   return (
-    <div className="wrapper">
+    <Wrapper>
 
-      <div className="column-left">
-        {loaded &&
-          <Header
-            attributes={airline.data.attributes}
-            reviews={airline.included}
-          />
-        }
+      <Column>
+        <Main>
+          {loaded &&
+            <Header
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+          }
+        </Main>
         <div className="reviews"></div>
-      </div>
-      <div className="column-right">
+      </Column>
+      <Column>
         <div className="review-form">Review Form Goes Here</div>
-      </div>
+      </Column>
 
-    </div>
+    </Wrapper>
   );
 };
 
--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,18 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+
+
+const Wrapper = styled.div`
+padding: 50px, 100px, 50px, 0;
+font-size: 30px;
+
+img {
+height: 60px;
+width: 60px;
+border-radius: 100%;
+margin-bottom: -8px;
+padding-right: 10px;
+padding-left: 10px;
+}
+`;
+const TotalReviews = styled.div`
+font-size: 18px;
+padding: 10px 0;`;
+
+const TotalOutOf = styled.div`
+font-size: 18px;
+font-weight: bold;
+padding: 10px 0;
+`;
+
 
 const Header = (props) => {
   const { name, image_url, avg_score } = props.attributes;
   const total = props.reviews.length;
 
   return (
-    <div className="wrapper">
+    <Wrapper>
       <h1><img src={image_url} alt={name} />{name}</h1>
       <div>
-        <div className="total-reviews">{total} User Reviews</div>
+        <TotalReviews>{total} User Reviews</TotalReviews>
         <div className="star-rating"></div>
-        <div className="total-out-of">{avg_score} out of 5</div>
+        <TotalOutOf>{avg_score} out of 5</TotalOutOf>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
