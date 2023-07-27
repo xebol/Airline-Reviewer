6dcb30056f3307c32d86fae21c5c9b285e8f1226
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-07-27 13:42:24 -0600

    add radio buttons for rating an airline

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 const ReviewForm = (props) => {
+  const ratingOptions = [5, 4, 3, 2, 1].map((score, index) => {
+    return (<>
+      <input type="radio" value={score} name="rating" onChange={() => {
+        console.log('selected:', score);
+      }} id={`rating-${score}`} />
+      <label></label>
+    </>
+    );
+  });
+
   return (
     <div className="wrapper">
       <form onSubmit={props.handleSubmit}>
@@ -9,12 +19,12 @@ const ReviewForm = (props) => {
           <input onChange={props.handleChange} value={props.review.title} type="text" name="title" placeholder="Review Title" />
         </div>
         <div className="field">
-          <input onChange={props.handleChange} value={props.review.description}  type="text" name="description" placeholder="Review Description" />
+          <input onChange={props.handleChange} value={props.review.description} type="text" name="description" placeholder="Review Description" />
         </div>
         <div className="field">
           <div className="rating-container">
             <div className="rating-title-text">Rate {props.attributes.name}</div>
-            [Star Rating Here]
+            {ratingOptions}
           </div>
         </div>
         <button type="submit">Submit Your Review!</button>
